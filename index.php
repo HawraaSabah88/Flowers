c28dface0a8459d59548aad3a20864e119551bb7
Hawraa Sabah <hawraasabah55@gmail.com>
2024-03-16 23:28:22 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel='stylesheet' href='fonts/fontawesome/css/font-awesome.min.css'/>
+<link rel='stylesheet'href='css/style.css'/>
+
+    <title>Flowers Delivered</title>
+</head>
+<body>
+<header>
+    <div class='div1'>
+      <img class='logo' src='image2/16.jpg'>
+    </div>
+    <div class='div2'>
+      <nav class='bar'> 
+     <a class='header1' href='files/h1.html'>BIRTHDAYS</a>
+     <a class='header1' href='files/h2.html'>SYMAPTHY</a>
+     <a class='header1' href='files/h3.html'>GIFTS & PLANTS</a>
+     <a class='header1' href='files/h4.html'>OCCASIONS</a>
+     <a class='header1' href='files/h5.html'>FLOWERS</a>   
+     </nav>
+    </div>
+   <div class='icons'>
+        <div  class='find'>
+            <i class="fa fa-search" ><br><br>SEARCH</i>
+         </div>
+         <div class='account'>
+             <i class="fa fa-user"><br><br>ACCOUNT</i>
+        </div>
+    </div>
+</header>
+    <main>
+    <div class="logo1" >
+    <img class='bbb' src='image2/t5.jpg' alt=' '>
+    </div>
+ 
+    <div class="logo2">
+        <div class="h">SHOP BY COLOR</div>
+        <div class="ph">
+            <div class="ph1"><img src='image2/1000.jpg'><div class="p"><a href='files/pink.html'> PINK</a></div></div>
+            <div class="ph2"><img src='image2/2000.jpg'><div class="p"><a href='files/red.html'> RED</a></div></div>
+            <div class="ph3"><img src='image2/3000.jpg'><div class="p"><a href='files/yellow.html'>YELLOW</a></div></div>
+            <div class="ph4"><img src='image2/4000.jpg'><div class="p"><a href='files/white.html'>WHITE</a></div></div>
+        </div>
+    </div>
+    <div class="logo4">
+  <div class="img1"> <img src="image2/sus.jpg" alt=""></div> 
+  <div class="img2">
+ <p> Here Comes </p><p>the Sun(Flowers)!</p>
+<p>Yellow symbolizes joy & friendship, making a bouquet of sunflowers the perfect gift</p>
+  </div>
+ </div>
+
+
+<div class="logo6">
+<div class="h2">Featured Sunflowers
+</div>
+</div>
+
+<div class="yu2">
+<div class="d1"></div>
+        <div class="d2">
+            <div class='left'> </div>
+            <div class='middle'>
+            <div class="card">
+                    <div class="img1"> <img src="image2/h1.jpg"> </div>
+                    <div class="d"><a href='#'>
+                       
+                    <p>    Sunflower Bouquet</p>
+                    <p> $39.99 - $59.99</p>
+                    <p> Shipped in a Gift Box</p></a>
+                 
+
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/h2.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Golden Sunflowers™ in Rustic Charm Vase</p>
+                        <p>  $59.99 - $79.99</p>
+                        <p>   Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/h3.jpg"> </div>
+                    <div class="d"><a href='#'>
+         
+                  <p>   Floral Embrace™</p>
+                  <p>  $49.99 - $69.99</p>
+                  <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+     
+                </div>
+            <div class='right'> </div>
+                      
+     
+        </div>
+        <div class="d3"></div>
+
+      </div>
+      </div>
+      <a href='files/sunflowers.html'>
+      <div class="bt"><button class='bt1'> SHOP ALL SUNFLOWERS</button>
+               </div></a>
+                  <div class="logo5">
+                   <div class="logo51"><img src="image2/world.jpg" alt=""></div>
+                   <div class="logo52">Like Flowers,we can also choose to bloom.</div>
+               </div>
+
+<div class="logo3">
+<div class="h2">BEST SELLING FLOWERS
+</div>
+</div>
+<div class="yu">
+<div class="d1"></div>
+        <div class="d2">
+            <div class='left'> </div>
+            <div class='middle'>
+            <div class="card">
+                    <div class="img1"> <img src="image2/1.jpg"> </div>
+                    <div class="d"><a href='#'>
+                       
+                    <p> Sunny Garden Basket™</p>
+                    <p> $39.99 - $59.99</p>
+                    <p> Same-Day Local Florist Delivery</p></a>
+
+
+
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/2.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Cherished Blooms Bouquet</p>
+                        <p>    $39.99 - $66.99</p>
+                        <p>    Shipped in a Gift Box</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/3.jpg"> </div>
+                    <div class="d"><a href='#'>
+         
+                  <p>   Fall Rose & Peruvian Lily Bouquet</p>
+                  <p>  $39.99 - $56.99</p>
+                  <p> Shipped in a Gift Box</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/4.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>  Exotic Breeze™ Orchids</p>
+                    <p>$44.99 - $83.99</p>
+                    <p>Shipped in a Gift Box</p></A>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/5.jpg"> </div>
+                    <div class="d"><a href='#'>
+                        <p> Fields of Europe™ Romance</p>
+                        <p>$49.99 - $79.99</p>
+                        <p>Same-Day Local Florist Delivery </p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/6.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    </p> Happy Gerbera Daisies, 12-24 Stems</p>
+                    <p>$39.99 - $71.99</p>
+                    <p>Shipped in a Gift Box</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/7.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Vibrant Blooms Bouquet</p>
+                    <p>$34.99 - $66.99</p>
+                    <p>Shipped in a Gift Box</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/8.jpg"> </div>
+                    <div class="d"><a href='#'>
+                       
+                    <p>Fields of Europe™ Bliss</p>
+                    <p>$39.99 - $69.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/9.jpg"> </div>
+                    <div class="d"><a href='#'>
+                      
+                    <p>Floral Treasures Bouquet™</p>
+                    <p>$39.99 - $59.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/10.jpg"> </div>
+                    <div class="d"><a href='#'>
+                       
+                    <p>Fields of Europe™ for Fall</p>
+                    <p>$39.99 - $69.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/11.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Adoring Love Bouquet™</p>
+                    <p>$99.99 - $124.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/12.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p> Floral Melody™ in White Mosaic Vase</p>
+                    <p>$49.99 - $59.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/13.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Shades of Purple™</p>
+                    <p>$59.99 - $79.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/14.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Amethyst Splendor™ Bouquet</p>
+                    <p>$49.99 - $69.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="img1"> <img src="image2/15.jpg"> </div>
+                    <div class="d"><a href='#'>
+                    <p>    Amber Waves™</p>
+                    <p>$49.99 - $59.99</p>
+                    <p>Same-Day Local Florist Delivery</p></a>
+                    </div>
+                </div>
+            </div>
+            <div class='right'> </div>
+        </div>
+        <div class="d3"></div>
+</div>
+</div>
+<div class="des">
+    <h1> Online Fresh Flower Delivery for 100 Years</h1>
+   <p>Established in 1910, FTD has been a premier provider of beautiful floral arrangements and gorgeous flower bouquets for over 100 years. Whatever the occasion, our evergreen collection of colorful stems, blossoming plants, and fragrant arrangements have warmed hearts all across the country. At FTD, we celebrate the little things in life and cherish the timeless charm a single flower embodies. From birthday flowers to sympathy flowers, all of our hand-picked flower arrangements are worth more than a thousand words.
+
+
+Our sprawling network of florists is chock-full of expert green-thumbs with an eye for elegance and perfection. As one of the largest and most trusted online floral retailers in the world, we take immense pride in our industry-leading title. Delivering quality, innovation, creative artistry, and dependability, FTD is much more than a flowery delivery service.
+</p>
+<h1>
+How To Send Same Day Flowers Online?</h1>
+<p>Last minute gifts are a simple part of life—especially when things get stressful and put onto the back burner. We offer expedited same day delivery on our flowers, plants, and gifts as long as you order by 2 P.M. in your recipient’s time zone. Available Monday through Friday, our same day flower delivery service ensures that your last minute gift is still on time!
+ 
+ Even though you may be in a rush to get your gift baskets or rose bouquet order in, each and every one of our arrangements are handled with painstaking care. No detail falls short under our expert florists’ watch. Our online ordering system makes it easy to select a bouquet or gift from our catalog, add any custom features, and send to your recipient’s address.
+ </p>
+<h1>
+ What Are The Perfect Flower Arrangements for Funerals?</h1>
+<p>Provide love and support to a family member or friend grieving the loss of a loved one with a thoughtful arrangement of funeral flowers. Commemorate the life and boundless love of the departed with one of our beautiful lily bouquets, or our many multi-flower wreaths, baskets, and easel arrangements.
+ 
+ Our bestselling sympathy funeral flowers feature pretty stems ranging from roses and daisies to hydrangeas and carnations. These exquisite bouquets of white, cream, lavender, pink, and purple are soft hues that express warm remembrance and eternal affection.
+ </p>
+<h1>
+How To Order Flower Arrangements for Any Occasion?</h1>
+
+<p>
+Ordering flowers for a specific occasion? At FTD, we categorize our collection of flower arrangements and plants by occasions to make your shopping experience simple. Whether you’re searching for a gentle tulip bouquet for an anniversary celebration, or a vibrant daisy bouquet for a milestone birthday, you can find everything you need and more when you buy flowers online with FTD.
+</p>
+
+
+<h1> How Much Does The FTD Online Flower Delivery Services Cost?</h1>
+
+<p> We offer arrangement, bouquet, and plant delivery services to almost 100% of the United States and Canada and have expanded our expertise to more than 150 countries across the globe. Within the United States, all of our specialty gifts, flowers, and plants are delivered by FedEx®, UPS®, or USPS®. Standard shipping and same day delivery service charges start as low as $17.99, though prices may vary per delivery location or calendar day. Delivery is available Monday through Saturday, with Sunday delivery available on select holidays.</p>
+</div>
+</main>
+<footer>
+    <div class="row">
+        <ul class='ft'>
+        <h4>campany Info</h4>
+        <a href='files/history.html'>   <li><i class='fa fa-angle-right'>history</i></li></a>
+            <a href='files/management.html'>  <li><i class='fa fa-angle-right'>management</i></li></a>
+        </ul>
+    </div>
+    <div class="row">
+        <ul class='ft'>
+       <h4>account</h4>
+            <a href='#'>  <li><i class='fa fa-angle-right'>login in</i></li></a>
+            <a href='#'> <li><i class='fa fa-angle-right'> login up</i></li></a>
+        </ul>
+    </div> 
+    <div class="row">
+   
+            <h4>conect</h4>
+          <a href='https://www.facebook.com/FTDflowers/'>  <i class='fa fa-facebook'></i></a>
+          <a href='https://www.instagram.com/FTDFlowers/'>   <i class='fa fa-instagram'></i></a>
+            <a href='https://twitter.com/FTDFlowers'>   <i class='fa fa-twitter-square'></i></a>
+            <a href='https://www.pinterest.com/FTDFlowers/'>   <i class='fa fa-pinterest'></i></a>
+        
+    </div>
+    <div class="hs">
+      <h4>  Made by Hawraa Sabah 2019   </h4>          <h4>Many thinks Professor Abdullah</h4>
+    </div>
+</footer>
+</body>
+</html>
